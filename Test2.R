f13798c1fa7a863b446e140af7b6a367f7ade4d5
isalineta <isaline.taillard@uzh.ch>
2023-02-08 10:58:25 +0100

    test 8.0

--- Test2.R
+++ Test2.R
@@ -4,3 +4,11 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:isalineta/R-test-Project.git
 git push -u origin main
+
+git config --global user.email "isaline.taillard@uzh.ch"
+git config --global user.name "isalineta"
+
+
+x=1
+y=2
+test = x*y
